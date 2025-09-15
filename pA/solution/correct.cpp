33bd70f2d1c13b07ee19be840ff53748306eabaa
linushuan <126330400+linushuan@users.noreply.github.com>
2025-09-15 19:47:59 +0800

    Update correct.cpp
    Add the correct answer of pA

--- pA/solution/correct.cpp
+++ pA/solution/correct.cpp
@@ -1,10 +1,30 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
+void solve() {
+    long long n, a, b;
+    cin >> n;
+    if(n % 2 == 1) {
+        a = (n + 1) / 2;
+        b = (n - 1) / 2;
+    }else if(n % 4 == 0) {
+        a = n / 4 + 1;
+        b = n / 4 - 1;
+    }else{
+        cout<<-1<<'\n';
+        return;
+    }
+    cout << a << " " << b <<'\n';
+    return;
+}
+
 int main() {
-	// TODO
-	int n;
-	cin >> n;
-	cout << 0 << endl;
-	return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
 }
