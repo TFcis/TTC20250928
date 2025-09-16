281a4fec59b547dd68909c0c9511e51d9bb84e83
linushuan <126330400+linushuan@users.noreply.github.com>
2025-09-17 00:08:16 +0800

    Update task1.cpp

--- pE/solution/task1.cpp
+++ pE/solution/task1.cpp
@@ -32,7 +32,7 @@ void solve() {
 	cin >> n >> m;
 	for(int i = 1; i <= n; i++) {
 		for(int j = 1; j <= m; j++) {
-			ans += phi[gcd(i, j)];
+			ans += phi[__gcd(i, j)];
 		}
 	}
 	cout << ans << '\n';
