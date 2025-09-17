eff21f98d729f37fe132d263ebfc2de03a6a074c
linushuan <linus971211@gmail.com>
2025-09-17 20:37:27 +0800

    fix pC checker

--- pC/checker/checker.cpp
+++ pC/checker/checker.cpp
@@ -6,24 +6,24 @@ int main(int argc, char* argv[]) {
 	registerTestlibCmd(argc, argv);
 	// about testlib, see https://codeforces.com/blog/entry/18291
     	bool ok = true;
-    	int n = ans.readInt();
+    	int n = ouf.readInt();
 	int len = inf.readInt();
 	inf.readEoln();
 	string anss = inf.readString();
 	if(anss.back() == '\n') anss.pop_back();
         if(n == -1) {
-            int b = ouf.readInt();
+            int b = ans.readInt();
             if(b != -1) {
                 ok = false;
             }
             ans.readEoln();
             ouf.readEoln();
         }else if(0 <= n && n <= 100000000){
-            ans.readEoln();
+            ouf.readEoln();
 	    string s(len, '0');
 	    for(int i = 0; i < n; i++) {
-	    	int p = ans.readInt();
-		ans.readEoln();
+	    	int p = ouf.readInt();
+		ouf.readEoln();
 		if(p == 1) {
 			s[0] = !((bool)(s[1] - '0')) + '0';
 		}else if(p == len) {
