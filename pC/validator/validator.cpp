99d01b92984f263cd95c2e3f884c394bd7c6e493
linushuan <linus971211@gmail.com>
2025-09-17 18:31:32 +0800

    fix pC validator?

--- pC/validator/validator.cpp
+++ pC/validator/validator.cpp
@@ -11,7 +11,11 @@ int main(int argc, char* argv[]) {
 	int n = inf.readInt(2, max_n, "n");
 	inf.readEoln();
 	if(diff) {
-		string s = inf.readString();
+		string s = "";
+		for(int i = 0; i < n; i++) {
+			char c = inf.readChar() - '0';
+			s += c;
+		}
 		for(int i = 1; i < n - 1; i++) {
 			ensure(s[i-1] != s[i] && s[i] != s[i+1]);
 		}
@@ -23,6 +27,5 @@ int main(int argc, char* argv[]) {
 	}
 	inf.readEoln();
 	inf.readEof();
-
 	return 0;
 }
