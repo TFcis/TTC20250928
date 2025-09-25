7b83f2180fddae1eb03452afafdbcd8666a641e6
linushuan <linus971211@gmail.com>
2025-09-25 22:21:16 +0800

    fix pB big.in

---
+++ pB/gen/manual/prin.py
@@ -0,0 +1,13 @@
+import random
+
+print(2)
+print(1000000, end = " ")
+print(1000000009)
+for i in range(999999):
+	print(random.randint(-2000000,2000000), end = " ")
+print(random.randint(-2000000,2000000))
+print(1000000, end = " ")
+print(1000000007)
+for i in range(999999):
+	print(random.randint(-2000000,2000000), end = " ")
+print(random.randint(-2000000,2000000))
